eb8107e253d6e06bac34c81d11b2be43777c0dc0
vande <vanderploeg.matthew@gmail.com>
2024-11-12 22:23:40 -0500

    merging main into auton code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftCloseAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftCloseAuton.java
@@ -32,6 +32,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -39,57 +40,42 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
-/*
- * This OpMode illustrates the concept of driving a path based on encoder counts.
- * The code is structured as a LinearOpMode
- *
- * The code REQUIRES that you DO have encoders on the wheels,
- *   otherwise you would use: RobotAutoDriveByTime;
- *
- *  This code ALSO requires that the drive Motors have been configured such that a positive
- *  power command moves them forward, and causes the encoders to count UP.
- *
- *   The desired path in this example is:
- *   - Drive forward for 48 inches
- *   - Spin right for 12 Inches
- *   - Drive Backward for 24 inches
- *   - Stop and close the claw.
- *
- *  The code is written using a method called: encoderDrive(speed, leftInches, rightInches, timeoutS)
- *  that performs the actual movement.
- *  This method assumes that each movement is relative to the last stopping place.
- *  There are other ways to perform encoder based moves, but this method is probably the simplest.
- *  This code uses the RUN_TO_POSITION mode to enable the Motor controllers to generate the run profile
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
+// Above this line in importing packages. Do not change anything above this line
+//
+//
+//
+//
+//
+//
+// The line below is what our auton in named.
 
-@Autonomous(name="LeftCloseAuton", group="Robot")
+@Autonomous(name="left close auton", group="Robot")
 //@Disabled
 public class LeftCloseAuton extends LinearOpMode {
 
     IMU imu;
 
     /* Declare OpMode members. */
+    // The lines below set up the new motors that we use.
     private DcMotor leftFrontDrive = null;
     private DcMotor leftBackDrive = null;
     private DcMotor rightFrontDrive = null;
     private DcMotor rightBackDrive = null;
     private ElapsedTime     runtime = new ElapsedTime();
+    private CRServo samplePickup = null;
+    private DcMotor towerMotor = null;
+    private DcMotor flipperMotor = null;
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    // Below this line is where we define our variables that we use for the program
 
-    // Calculate the COUNTS_PER_INCH for your specific drive train.
-    // Go to your motor vendor website to determine your motor's COUNTS_PER_MOTOR_REV
-    // For external drive gearing, set DRIVE_GEAR_REDUCTION as needed.
-    // For example, use a value of 2.0 for a 12-tooth spur gear driving a 24-tooth spur gear.
-    // This is gearing DOWN for less speed and more torque.
-    // For gearing UP, use a gear ratio less than 1.0. Note this will affect the direction of wheel rotation.
-    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
-    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // No External Gearing.
-    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = 42.5;
-    static final double     DRIVE_SPEED             = 0.6;
-    static final double     TURN_SPEED              = 0.5;
     double                  CURRENT_YAW             = 0.0;
     double                  robotDesiredDirection   = 0.0;
     double                  directionError          = 0.0;
@@ -104,6 +90,11 @@ public class LeftCloseAuton extends LinearOpMode {
         leftBackDrive  = hardwareMap.get(DcMotor.class, "left_back_drive");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
         rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
+        samplePickup = hardwareMap.get(CRServo.class, "sample_pickup");
+        towerMotor = hardwareMap.get(DcMotor.class, "tower_motor");
+        flipperMotor = hardwareMap.get(DcMotor.class, "flipper_motor");
+
+
 
         imu = hardwareMap.get(IMU.class, "imu");
 
@@ -125,22 +116,32 @@ public class LeftCloseAuton extends LinearOpMode {
         leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        towerMotor.setDirection(DcMotor.Direction.REVERSE);
 
         //Set the motors to brake when there is no power
         leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        towerMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        flipperMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        //Reset the encoders
         leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        towerMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+
+        //Set the motors to run using encoder mode
         leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         leftBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        towerMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        towerMotor.setTargetPosition(0);
+
 
         // Send telemetry message to indicate successful Encoder reset
         telemetry.addData("Starting at",  "%7d : %7d : %7d : %7d" ,
@@ -150,50 +151,51 @@ public class LeftCloseAuton extends LinearOpMode {
                           rightBackDrive.getCurrentPosition());
         telemetry.update();
 
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
         // Wait for the game to start (driver presses START)
         waitForStart();
 
-        // Step through each leg of the path,
+        // Step through each leg of the path, this is what our robot is actually doing
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
 
-        turnLeft(0.5, 90);
+        //lift(4250,1);
+        driveForward(.5,14.0,10.0);
+        intake(1.0);
+
+
 
         //        encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
 //        encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
-        while (opModeIsActive()){
-            driveForward(0.5,25,30);
-            //sampleTower place preloaded specimen
-            sleep(3000);
-            turnLeft(0.5,90);
-            driveForward(0.5,48,30);
-            turnLeft(0.5,-90);
-            //samplePickup pick up neutral sample
-            sleep(3000);
-            turnLeft(0.5,180.0);
-            driveForward(0.5,10,30);
-            //sampleTower up, sample into high basket
-            sleep(2000);
-            turnLeft(0.5,180);
-            driveForward(0.5,12.0, 3.0);
-            turnLeft(0.5,180.0);
-
-            sleep(30000);
-
-            getYaw();
-            telemetry.addData("Path", "Complete");
-            telemetry.update();
-        }
+//        while (opModeIsActive()){
+//            driveForward(0.5,20.0,30.0);
+//            turnLeft(0.5, -90.0);
+//            getYaw();
+//            telemetry.addData("Path", "Complete");
+//            telemetry.update();
+//        }
         sleep(100000);  // pause to display final telemetry message.
     }
 
-    /*
-     *  Method to perform a relative move, based on encoder counts.
-     *  Encoders are not reset as the move is based on the current position.
-     *  Move will stop if any of three conditions occur:
-     *  1) Move gets to the desired position
-     *  2) Move runs out of time
-     *  3) Driver stops the OpMode running.
-     */
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    // Everything below this line is functions that we use in our main program.
+
     public void driveForward(double speed,
                              double forwardInches,
                              double timeoutS) {
@@ -202,28 +204,17 @@ public class LeftCloseAuton extends LinearOpMode {
         // Ensure that the OpMode is still active
         if (opModeIsActive()) {
 
-            // Determine new target position, and pass to motor controller
+            // Determine new target position
             newFrontLeftTarget = leftFrontDrive.getCurrentPosition() + (int)(forwardInches * COUNTS_PER_INCH);
-
-            // Turn On RUN_TO_POSITION
-            //leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            //leftBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            //rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            //rightBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
             // reset the timeout time and start motion.
             runtime.reset();
-            leftFrontDrive.setPower(Math.abs(speed));
-            leftBackDrive.setPower(Math.abs(speed));
-            rightFrontDrive.setPower(Math.abs(speed));
-            rightBackDrive.setPower(Math.abs(speed));
+//            leftFrontDrive.setPower(Math.abs(speed));
+//            leftBackDrive.setPower(Math.abs(speed));
+//            rightFrontDrive.setPower(Math.abs(speed));
+//            rightBackDrive.setPower(Math.abs(speed));
 
-            // keep looping while we are still active, and there is time left, and both motors are running.
-            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
-            // its target position, the motion will stop.  This is "safer" in the event that the robot will
-            // always end the motion as soon as possible.
-            // However, if you require that BOTH motors have finished their moves before the robot continues
-            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            //If we are running forward
             if(forwardInches>0.0) {
                 while (opModeIsActive() &&
                         (runtime.seconds() < timeoutS) &&
@@ -231,9 +222,9 @@ public class LeftCloseAuton extends LinearOpMode {
                     getYaw();
                     // Below: Ensuring that that the robot corrects itself correctly if the
                     // target angle is close to 180 or -180, and the robot crosses that threshold
-                    if (robotDesiredDirection > 130){
-                        if (CURRENT_YAW < 0) {
-                            directionError = CURRENT_YAW +360 - robotDesiredDirection;
+                    if (robotDesiredDirection > 130.0){
+                        if (CURRENT_YAW < 0.0) {
+                            directionError = CURRENT_YAW + 360.0 - robotDesiredDirection;
                         }
                         else {
                             directionError = CURRENT_YAW - robotDesiredDirection;
@@ -250,10 +241,10 @@ public class LeftCloseAuton extends LinearOpMode {
                     else {
                         directionError = CURRENT_YAW - robotDesiredDirection;
                     }
-                    //directionError = CURRENT_YAW - robotDesiredDirection;
 
                     directionCorrectionModifier = directionError * 0.01;
                     telemetry.addData("modifier", "%.2f Deg. (Heading)", directionCorrectionModifier);
+                    telemetry.addData("encoder position", "%d Deg. (Heading)", rightFrontDrive.getCurrentPosition());
                     telemetry.update();
                     leftFrontDrive.setPower(speed + directionCorrectionModifier);
                     leftBackDrive.setPower(speed + directionCorrectionModifier);
@@ -285,15 +276,17 @@ public class LeftCloseAuton extends LinearOpMode {
             rightFrontDrive.setPower(0);
             rightBackDrive.setPower(0);
 
-            // Turn off RUN_TO_POSITION
-            leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            leftBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            rightBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
             sleep(250);   // optional pause after each move.
         }
     }
+
+    // Above is the DriveForward Function
+    //
+    //
+    //
+    //
+    //
+    // Below is the TurnLeft Function
 
     public void turnLeft(double turnSpeed,
                         double leftAngle){
@@ -328,29 +321,7 @@ public class LeftCloseAuton extends LinearOpMode {
                 };
         }
         sleep(250);
-//        leftFrontDrive.setPower(0.0);
-//        leftBackDrive.setPower(0.0);
-//        rightFrontDrive.setPower(0.0);
-//        rightBackDrive.setPower(0.0);
-//
-//        if(CURRENT_YAW > robotDesiredDirection){
-//            while (opModeIsActive() && CURRENT_YAW > robotDesiredDirection + 1.0){
-//                getYaw();
-//                leftFrontDrive.setPower(0.1);
-//                leftBackDrive.setPower(0.1);
-//                rightFrontDrive.setPower(-0.1);
-//                rightBackDrive.setPower(-0.1);
-//            }
-//        }
-//        else{
-//            while (opModeIsActive() && CURRENT_YAW < robotDesiredDirection - 1.0){
-//                getYaw();
-//                leftFrontDrive.setPower(-0.1);
-//                leftBackDrive.setPower(-0.1);
-//                rightFrontDrive.setPower(0.1);
-//                rightBackDrive.setPower(0.1);
-//            }
-//        }
+
         while(opModeIsActive() && (Math.abs(CURRENT_YAW - robotDesiredDirection) > 1.0)) {
             getYaw();
             // modify direction error if we are close to 180 or -180
@@ -389,10 +360,44 @@ public class LeftCloseAuton extends LinearOpMode {
 
     }
 
+    // Above is the TurnLeft Function
+    //
+    //
+    //
+    //
+    //
+    //
+    // Below is the getYaw Function
+
     public void getYaw(){
         YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
         CURRENT_YAW = orientation.getYaw(AngleUnit.DEGREES);
         telemetry.addData("Yaw (Z)", "%.2f Deg. (Heading)", CURRENT_YAW);
         telemetry.update();
+    }
+
+
+
+    //intake mechanism function
+    public void intake(double intakePower){
+        samplePickup.setPower(-intakePower);
+    }
+
+
+
+    //lift Function
+    public void lift(int liftHeight, double liftPower){
+        towerMotor.setTargetPosition(liftHeight);
+        towerMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        towerMotor.setPower(liftPower);
+    }
+
+
+    //arm function
+    public void arm(int armPosition, double armPower){
+        flipperMotor.setTargetPosition(armPosition);
+        flipperMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        flipperMotor.setPower(armPower);
+
     }
 }
